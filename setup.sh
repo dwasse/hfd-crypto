f240b9200a39d9feaf6d328c8c8b3bda4f5f0c77
Daniel Wasserman <wassermandaniel8@gmail.com>
2019-09-09 18:21:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ sudo apt-get -y install postgresql
 sudo apt-get -y install python-psycopg2
 sudo apt-get -y install libpq-dev
 sudo apt-get -y install python3-pip
-sudo apt-get install python3-venv
-sudo pip3 install venv
+pip3 install -r requirements.txt
 service postgresql start
